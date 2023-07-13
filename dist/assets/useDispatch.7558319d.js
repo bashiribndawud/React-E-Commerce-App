0ca0cf454f55d04015618642edf4b02871cc98e7
Bashir Dauda <56880803+bashiribndawud@users.noreply.github.com>
2023-07-13 14:36:33 +0100

    published app2

---
+++ dist/assets/useDispatch.7558319d.js
@@ -0,0 +1,1 @@
+import{R as e,n as u,r as c}from"./index.ee93b54c.js";function r(t=e){const o=t===e?u:()=>c.exports.useContext(t);return function(){const{store:s}=o();return s}}const a=r();function i(t=e){const o=t===e?a:r(t);return function(){return o().dispatch}}const x=i();export{x as u};
---
+++ dist/assets/utils.631e3966.js
@@ -0,0 +1,1 @@
+function n(t){return t.reduce((e,r)=>r.quantity+e,0)}function a(t){return t.reduce((e,{product:r,quantity:u})=>r.price*u+e,0)}export{n as a,a as g};
