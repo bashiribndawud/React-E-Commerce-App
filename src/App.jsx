7071a7fd4aaf8597d4c9b7f3d229914a87e92d91
Bashir Dauda <56880803+bashiribndawud@users.noreply.github.com>
2023-07-13 14:33:05 +0100

    publish app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import React, { useState, Suspense, Fragment, lazy } from "react";
 import Button from "@mui/material/Button";
-import { createBrowserRouter, Navigate } from "react-router-dom";
-import { Route } from "react-router-dom";
-import { createRoutesFromElements } from "react-router-dom";
+import {
+  Route,
+  Routes,
+  BrowserRouter,
+  createRoutesFromElements,
+  createBrowserRouter,
+  Navigate,
+} from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-import Checkout from "./pages/Checkout";
-import Layouts from "./components/Layouts.jsx";
+const Home = lazy(() => import("./pages/Home"));
+const Cart = lazy(() => import("./pages/Cart"));
+const Login = lazy(() => import("./pages/Login"));
+const Checkout = lazy(() => import("./pages/Checkout"));
+const Layouts = lazy(() => import("./components/Layouts.jsx"));
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import AuthProvider, { useAuth } from "./firebase/Auth";
@@ -16,43 +21,59 @@ import Register from "./pages/Register";
 // import './App.css'
 
 // proctect pages against unathorized users
-function ProtectedRoute({children}){
-  const {user} = useAuth();
-  if(!user){
-    return <Navigate to={"/login"} />
+function ProtectedRoute({ children }) {
+  const { user } = useAuth();
+  if (!user) {
+    return <Navigate to={"/login"} />;
   }
-  return children
+  return children;
 }
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/">
-        <Route element={<Layouts />}>
-          <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route
-            path="/checkout"
-            element={
-              <ProtectedRoute>
-                <Checkout />
-              </ProtectedRoute>
-            }
-          />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Route>
-    </>
-  )
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <>
+//       <Routes path="/" element={<Layouts />}>
+//         <Route index element={<Home />} />
+//         <Route path="home" element={<Home />} />
+//         <Route path="cart" element={<Cart />} />
+//         <Route
+//           path="checkout"
+//           element={
+//             <ProtectedRoute>
+//               <Checkout />
+//             </ProtectedRoute>
+//           }
+//         />
+//       </Routes>
+//       <Route path="login" element={<Login />} />
+//       <Route path="register" element={<Register />} />
+//     </>
+//   )
+// );
 
 function App() {
   return (
     <AuthProvider>
       <Provider store={store}>
-        <RouterProvider router={router} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layouts />}>
+              <Route index element={<Home />} />
+              <Route path="home" element={<Home />} />
+              <Route path="cart" element={<Cart />} />
+              <Route
+                path="checkout"
+                element={
+                  <ProtectedRoute>
+                    <Checkout />
+                  </ProtectedRoute>
+                }
+              />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
       </Provider>
     </AuthProvider>
   );
